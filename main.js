ffa4c18ed8dd640f54ebaeac861f18caf2a0a5f6
Dean Haider <personal.haider@gmail.com>
2019-06-23 06:50:59 -0400

    Function to start game

--- main.js
+++ main.js
@@ -7,63 +7,88 @@ var wordsList = [
     "jordan",
     "ari",
     "maja",
-  ];
+];
+
 
 var chosenWord = "";
 
     var lettersInChosenWord = [];
     var blanksAndSuccesses = [];
     var wrongGuesses = [];
+    var numBlanks = 0;
+    var winCounter = 0;
+    var lossCounter = 0;
+    var numGuesses = 10;
 
-  var numBlanks = 0;
-  var winCounter = 0;
-  var lossCounter = 0;
-  var numGuesses = 10;
 
-
-        function startGame() {
+    function startGame() {
         numGuesses = 10;
-
-            chosenWord = wordsList[Math.floor(Math.random() * wordsList.length)];
-            lettersInChosenWord = chosenWord.split("");
-            numBlanks = lettersInChosenWord.length;
-            console.log(chosenWord);
-            blanksAndSuccesses = [];
-            wrongGuesses = [];
-  
-            for (var i = 0; i < numBlanks; i++) {
-            blanksAndSuccesses.push("_");
-            }
-  
+        chosenWord = wordsList[Math.floor(Math.random() * wordsList.length)];
+        lettersInChosenWord = chosenWord.split("");
+        numBlanks = lettersInChosenWord.length;
+        console.log(chosenWord);
+        blanksAndSuccesses = [];
+        wrongGuesses = [];
+        console.log(blanksAndSuccesses);
     
-    console.log(blanksAndSuccesses);
-    document.getElementById("guesses-left").innerHTML = numGuesses;
-    document.getElementById("word-blanks").innerHTML = blanksAndSuccesses.join(" ");
-  
-    document.getElementById("wrong-guesses").innerHTML = wrongGuesses.join(" ");
-  }
+        document.getElementById("guesses-left").innerHTML = numGuesses;
+        document.getElementById("word-blanks").innerHTML = blanksAndSuccesses.join(" ");
+        document.getElementById("wrong-guesses").innerHTML = wrongGuesses.join(" ");
+
+        for (var i = 0; i < numBlanks; i++) {
+            blanksAndSuccesses.push("_");
+        }
+    }
+    
     
     function checkLetters(letter) {
         var letterInWord = false;
 
             for (var i = 0; i < numBlanks; i++) {
-            if (chosenWord[i] === letter) {
-            letterInWord = true;
+                    if (chosenWord[i] === letter) {
+                        letterInWord = true;
+                    }
             }
-        }
   
-        if (letterInWord) {
-            for (i = 0; i < numBlanks; i++) {
-            if (chosenWord[i] === letter) {
-            blanksAndSuccesses[i] = letter;
+            if (letterInWord) {
+                for (i = 0; i < numBlanks; i++) {
+                    if (chosenWord[i] === letter) {
+                        blanksAndSuccesses[i] = letter;
+                    }
+                }
+                console.log(blanksAndSuccesses);
             }
-        }
-       
-        console.log(blanksAndSuccesses);
+
+            else {
+                wrongGuesses.push(letter);
+                numGuesses--;
+            }
     }
 
-    else {
-        wrongGuesses.push(letter);
-        numGuesses--;
+
+    function gamebegins() {
+        document.getElementById("guesses-left").innerHTML = numGuesses;
+        document.getElementById("word-blanks").innerHTML = blanksAndSuccesses.join(" ");
+        document.getElementById("wrong-guesses").innerHTML = wrongGuesses.join(" ");
+  
+    
+        if (lettersInChosenWord.toString() === blanksAndSuccesses.toString()) {
+            winCounter++;
+            alert("Winner!");
+            document.getElementById("win-counter").innerHTML = winCounter;
+            startGame();
+        }
+  
+        else if (numGuesses === 0) {
+            lossCounter++;
+            alert("Loser");
+            document.getElementById("loss-counter").innerHTML = lossCounter;
+            startGame();
+        }
     }
-  }
+    
+    document.onkeyup = function(restartgame) {
+        var guessedletter = String.fromCharCode(restartgame.keyCode).toLowerCase();
+        checkLetters(guessedletter);
+        gamebegins();
+    };
